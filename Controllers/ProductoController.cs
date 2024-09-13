c81c67fb005a86e39db2ce0acfe4b58354c4c905
SimonArias77 <sariass077@gmail.com>
2024-09-12 22:10:11 -0500

    feat:se crea metodo post

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -5,25 +5,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApiCrud.Data;
+using WebApiCrud.Models;
 
-namespace WebApiCrud.Controllers
+namespace WebApiCrud.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductoController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductoController : ControllerBase
+    private readonly ApplicationDbContext _context;
+    public ProductoController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-        public ProductoController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-        // GET: api/Producto
-        [HttpGet]
+        _context = context;
+    }
+    // GET: api/Producto
+    [HttpGet]
     public async Task<IActionResult> GetProducto()
     {
         return Ok(await _context.Productos.ToListAsync());
-            }
-//get, post, delete,put
-       
+    }
+    //POST: api/Producto
+    [HttpPost]
+    public async Task<IActionResult> PostProducto(Producto producto)
+    {
+        await  _context.Productos.AddAsync(producto);
+        await _context.SaveChangesAsync();
+        return Ok("producto guardado con éxito");
+         
     }
 }
+
+
+//Get (leer u obtener datos desde el servidor, se pueden obtener todos los productos o solo uno por ID)
+//Post (se usa para crear un nuevo recurso en el servidor)
+//Delete ( se usa para eliminar un recurso existente en el servidor) 
+//Put (se usa para actualizar un recurso existente, generalmente para reemplazar)
+
+
+
+
